176f5b29d5f2d90dc0114d8457cb8e677d98a278
ZhangKuixun <1129826190@qq.com>
2018-11-22 19:29:41 +0800

    no message

--- utilcode/src/test/java/com/blankj/utilcode/util/reflect/ReflectUtilsTest.java
+++ utilcode/src/test/java/com/blankj/utilcode/util/reflect/ReflectUtilsTest.java
@@ -1,5 +1,7 @@
 package com.blankj.utilcode.util.reflect;
 
+import com.blankj.utilcode.util.EncodeUtils;
+import com.blankj.utilcode.util.EncryptUtils;
 import com.blankj.utilcode.util.ReflectUtils;
 
 import org.hamcrest.Matchers;
